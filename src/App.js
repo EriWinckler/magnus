d470bf1d5df82e9120cd0d7e782332bccb940bc0
Eridan <efwinckler@gmail.com>
2020-07-26 23:24:41 -0400

    added components and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style/App.scss';
+import Header from './components/header';
+import Footer from './components/footer';
+import About from './components/about';
+import Resume from './components/resume';
+import Contact from './components/contact';
+import Portfolio from './components/portfolio';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {/* <About />
+        <Resume /> 
+        <Portfolio /> */}
+        <Contact />
+        {/* <Footer /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
