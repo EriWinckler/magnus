212cc7ed44ee1a4ea2ebbcd5acefaf20c3880a6a
EriWinckler <efwinckler@gmail.com>
2021-06-01 22:59:17 -0400

    fixed navbar responsiviness

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,21 +7,25 @@ const navigation = () => {
       className="bs-navbar-collapse"
       bg="light"
       variant="light"
-      sticky="top"
+      fixed="top"
+      expand="lg"
+      collapseOnSelect
     >
-      <Navbar.Brand href="#home" className="navbarBrand">
-        <img src={require("../images/icon-48x48.png")} alt="logo" />
-      </Navbar.Brand>
-
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#resume">Education</Nav.Link>
-        <Nav.Link href="#certificates">Certificates</Nav.Link>
-        <Nav.Link href="#languages">Skills</Nav.Link>
-        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
-      </Nav>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Brand href="#home" className="navbarBrand">
+          <img src={require("../images/icon-48x48.png")} alt="logo" />
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#resume">Education</Nav.Link>
+          <Nav.Link href="#certificates">Certificates</Nav.Link>
+          <Nav.Link href="#languages">Skills</Nav.Link>
+          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
