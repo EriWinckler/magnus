1f092b60aeded6a35a4eba00e92092cab0d4e1bc
EriWinckler <efwinckler@gmail.com>
2021-06-16 15:29:30 -0400

    added new item on portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -6,45 +6,84 @@ const Portfolio = () => {
     <section id="portfolio">
       <div>
         <h1>Check Out My Work.</h1>
-        <Tilt className="Tilt" options={{ max: 25 }} style={{ width: 300 }}>
-          <div className="Tilt-inner">
-            <a
-              href="https://brazilboutique-ba097.firebaseapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="brBoutiqueLink"
-            >
-              <img
-                src={require("../images/brazilBoutique.jpg")}
-                alt="store screenshot"
-                className="brazilBoutique"
-              />
-            </a>
-          </div>
-        </Tilt>
-        <a
-          href="https://brazilboutique-ba097.firebaseapp.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="brBoutiqueLink"
-        />
-        <p className="brazilBoutique-p">
-          E-Commerce page for a customer from Brazil
-        </p>
+        <div>
+          <Tilt className="Tilt" options={{ max: 25 }} style={{ width: 300 }}>
+            <div className="Tilt-inner">
+              <a
+                href="https://brazilboutique-ba097.firebaseapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="brBoutiqueLink"
+              >
+                <img
+                  src={require("../images/brazilBoutique.jpg")}
+                  alt="store screenshot"
+                  className="brazilBoutique"
+                />
+              </a>
+            </div>
+          </Tilt>
+          <a
+            href="https://brazilboutique-ba097.firebaseapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="brBoutiqueLink"
+          />
+          <p className="brazilBoutique-p">
+            E-Commerce page for a customer from Brazil
+          </p>
 
-        <p className="timeline">
-          Page using bootstrap, React Router, React Currency Format. <br />
-          Hosted with Firebase that also works as user authenticator.
-        </p>
+          <p className="timeline">
+            Page using bootstrap, React Router, React Currency Format. <br />
+            Hosted with Firebase that also works as user authenticator.
+          </p>
 
-        <a
-          href="https://github.com/EriWinckler/BrazilBoutique"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="brazilButton"
-        >
-          <i className="fa fa-github"></i> Brazil Boutique
-        </a>
+          <a
+            href="https://github.com/EriWinckler/BrazilBoutique"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="brazilButton"
+          >
+            <i className="fa fa-github"></i> Brazil Boutique
+          </a>
+
+          <Tilt className="Tilt" options={{ max: 25 }} style={{ width: 300 }}>
+            <div className="Tilt-inner">
+              <a
+                href="https://eriscrudapp.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="brBoutiqueLink"
+              >
+                <img
+                  src={require("../images/CRUDpage.png")}
+                  alt="CRUD page screenshot"
+                  className="brazilBoutique"
+                />
+              </a>
+            </div>
+          </Tilt>
+          <a
+            href="https://eriscrudapp.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="brBoutiqueLink"
+          />
+          <p className="brazilBoutique-p">CRUD App</p>
+
+          <p className="timeline">
+            Simple CRUD (Create, Read, Update and Delete) page.
+          </p>
+
+          <a
+            href="https://github.com/EriWinckler/CRUD-App"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="CRUDButton"
+          >
+            <i className="fa fa-github"></i> CRUD App
+          </a>
+        </div>
       </div>
     </section>
   );
