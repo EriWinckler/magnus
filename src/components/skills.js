b2b7170022476daea3835f9ea1119671734c4437
Eri <efwinckler@gmail.com>
2021-09-20 19:12:09 -0400

    updated Java skillbar

--- src/components/skills.js
+++ src/components/skills.js
@@ -24,7 +24,7 @@ const Skills = () => {
           <li>
             <span
               style={{
-                width: "60%",
+                width: "75%",
                 backgroundColor: getRandomColor(),
               }}
               className="bar-expand html"
