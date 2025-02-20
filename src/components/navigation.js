469534c41ba4e28b2e5028bee6bdc4f78bf4f958
EriWinckler <efwinckler@gmail.com>
2025-02-19 22:23:58 -0500

    centered navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const navigation = () => {
   return (
@@ -11,21 +11,26 @@ const navigation = () => {
       expand="lg"
       collapseOnSelect
     >
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
+      <Container>
         <Navbar.Brand href="#home" className="navbarBrand">
           <img src={require("../images/icon-48x48.png")} alt="logo" />
         </Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#resume">Education</Nav.Link>
-          <Nav.Link href="#certificates">Certificates</Nav.Link>
-          <Nav.Link href="#languages">Skills</Nav.Link>
-          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className="ml-auto"
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#resume">Education</Nav.Link>
+            <Nav.Link href="#certificates">Certificates</Nav.Link>
+            <Nav.Link href="#languages">Skills</Nav.Link>
+            {/* <Nav.Link href="#portfolio">Portfolio</Nav.Link> */}
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
