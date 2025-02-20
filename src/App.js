a176347de4bc5e160b6400805ec4db0d24df0fa6
EriWinckler <efwinckler@gmail.com>
2025-02-19 22:23:50 -0500

    removed portfolio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./style/App.scss";
-
-// Google Analytics
-import ReactGA from "react-ga";
 
 // Components
 import Header from "./components/header";
@@ -10,17 +7,11 @@ import Footer from "./components/footer";
 import About from "./components/about";
 import Resume from "./components/resume";
 import Contact from "./components/contact";
-import Portfolio from "./components/portfolio";
 import Skills from "./components/skills";
 import Certificates from "./components/certificates";
 import Navigation from "./components/navigation";
 
 const App = () => {
-  // useEffect(() => {
-  //   ReactGA.initialize("UA-180197702-1");
-  //   ReactGA.pageview(window.location.pathname);
-  // });
-
   return (
     <div className="App">
       <Navigation />
@@ -29,7 +20,7 @@ const App = () => {
       <Resume />
       <Certificates />
       <Skills />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <Contact />
       <Footer />
     </div>
